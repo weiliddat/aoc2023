3b45e5935f0728c8fde7d22156614c71a8e33627
weiliddat <ongchiawei@gmail.com>
2023-12-01 11:35:19 +0100

    day01

--- day01/day01.go
+++ day01/day01.go
@@ -41,25 +41,68 @@ func Part01(input string) (string, error) {
 func Part02(input string) (string, error) {
 	lines := strings.Split(input, "\n")
 
-	replacer := strings.NewReplacer(
-		"one", "1",
-		"two", "2",
-		"three", "3",
-		"four", "4",
-		"five", "5",
-		"six", "6",
-		"seven", "7",
-		"eight", "8",
-		"nine", "9",
-	)
-
-	replacedInput := ""
-	for _, line := range lines {
-		replacedInput += replacer.Replace(line)
-		replacedInput += "\n"
+	digitLookup := map[string]int{
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
 	}
 
-	return Part01(replacedInput)
+	sum := 0
+
+	for _, line := range lines {
+		firstValue, firstIndex, lastValue, lastIndex := 0, -1, 0, -1
+
+		for dK, dV := range digitLookup {
+			index := strings.Index(line, dK)
+
+			if index == -1 {
+				continue
+			}
+
+			if firstIndex == -1 {
+				firstIndex = index
+				firstValue = dV * 10
+			} else if index <= firstIndex {
+				firstIndex = index
+				firstValue = dV * 10
+			}
+
+			index = strings.LastIndex(line, dK)
+
+			if index == -1 {
+				continue
+			}
+
+			if lastIndex == -1 {
+				lastIndex = index
+				lastValue = dV
+			} else if index >= lastIndex {
+				lastIndex = index
+				lastValue = dV
+			}
+		}
+
+		sum = sum + firstValue + lastValue
+	}
+
+	result := strconv.Itoa(sum)
+
+	return result, nil
 }
 
 func Solve(input string) (string, string, error) {
