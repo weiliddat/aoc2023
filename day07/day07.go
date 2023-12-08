c31a972b55fbc014bea2453a6beb2ec466beaff9
weiliddat <ongchiawei@gmail.com>
2023-12-08 23:50:31 +0100

    day07 part02

--- day07/day07.go
+++ day07/day07.go
@@ -62,34 +62,35 @@ var CardLookup = map[string]int{
 	"2": 2,
 }
 
+var JokerCardLookup = map[string]int{
+	"A": 14,
+	"K": 13,
+	"Q": 12,
+	"T": 10,
+	"9": 9,
+	"8": 8,
+	"7": 7,
+	"6": 6,
+	"5": 5,
+	"4": 4,
+	"3": 3,
+	"2": 2,
+	"J": 1,
+}
+
 func Part01(input string) (string, error) {
 	lines := aoc_util.SplitLines(input)
 
 	hands := []Hand{}
 	for _, line := range lines {
-		hand, err := ParseHand(&line)
+		hand, err := ParseHand(&line, false)
 		if err != nil {
 			return "", err
 		}
 		hands = append(hands, hand)
 	}
 
-	slices.SortFunc(hands, func(a, b Hand) int {
-		if a.Type != b.Type {
-			return int(a.Type - b.Type)
-		}
-
-		diff := 0
-		for i := 0; i < len(a.Cards); i++ {
-			aValue := CardLookup[a.Cards[i:i+1]]
-			bValue := CardLookup[b.Cards[i:i+1]]
-			if aValue != bValue {
-				diff = aValue - bValue
-				break
-			}
-		}
-		return diff
-	})
+	slices.SortFunc(hands, SortHands)
 
 	sum := 0
 	for index, hand := range hands {
@@ -100,10 +101,28 @@ func Part01(input string) (string, error) {
 }
 
 func Part02(input string) (string, error) {
-	return "", nil
+	lines := aoc_util.SplitLines(input)
+
+	hands := []Hand{}
+	for _, line := range lines {
+		hand, err := ParseHand(&line, true)
+		if err != nil {
+			return "", err
+		}
+		hands = append(hands, hand)
+	}
+
+	slices.SortFunc(hands, SortHandsJoker)
+
+	sum := 0
+	for index, hand := range hands {
+		sum += (index + 1) * hand.Bid
+	}
+
+	return strconv.Itoa(sum), nil
 }
 
-func ParseHand(input *string) (Hand, error) {
+func ParseHand(input *string, useJoker bool) (Hand, error) {
 	var hand Hand
 
 	cardText, bidText, _ := strings.Cut(*input, " ")
@@ -121,25 +140,65 @@ func ParseHand(input *string) (Hand, error) {
 		hand.Counts[char]++
 	}
 
-	counts := []int{}
-	for _, count := range hand.Counts {
-		counts = append(counts, count)
+	counts := []struct {
+		count int
+		card  string
+	}{}
+	for card, count := range hand.Counts {
+		counts = append(counts, struct {
+			count int
+			card  string
+		}{card: card, count: count})
 	}
-	slices.Sort(counts)
-	slices.Reverse(counts)
 
-	if counts[0] == 5 {
+	slices.SortFunc(counts, func(a, b struct {
+		count int
+		card  string
+	}) int {
+		return b.count - a.count
+	})
+
+	if useJoker {
+		jokers, found := hand.Counts["J"]
+		if found {
+			usedJoker := false
+			if counts[0].card != "J" {
+				counts[0].count += jokers
+				usedJoker = true
+			} else if len(counts) > 1 {
+				counts[1].count += jokers
+				usedJoker = true
+			}
+
+			if usedJoker {
+				for index, count := range counts {
+					if count.card == "J" {
+						counts[index].count -= jokers
+					}
+				}
+			}
+		}
+	}
+
+	slices.SortFunc(counts, func(a, b struct {
+		count int
+		card  string
+	}) int {
+		return b.count - a.count
+	})
+
+	if counts[0].count == 5 {
 		hand.Type = Fives
-	} else if counts[0] == 4 {
+	} else if counts[0].count == 4 {
 		hand.Type = Fours
-	} else if counts[0] == 3 {
-		if counts[1] == 2 {
+	} else if counts[0].count == 3 {
+		if counts[1].count == 2 {
 			hand.Type = FullHouse
 		} else {
 			hand.Type = Threes
 		}
-	} else if counts[0] == 2 {
-		if counts[1] == 2 {
+	} else if counts[0].count == 2 {
+		if counts[1].count == 2 {
 			hand.Type = TwoPair
 		} else {
 			hand.Type = OnePair
@@ -149,4 +208,38 @@ func ParseHand(input *string) (Hand, error) {
 	}
 
 	return hand, nil
+}
+
+func SortHands(a, b Hand) int {
+	if a.Type != b.Type {
+		return int(a.Type - b.Type)
+	}
+
+	diff := 0
+	for i := 0; i < len(a.Cards); i++ {
+		aValue := CardLookup[a.Cards[i:i+1]]
+		bValue := CardLookup[b.Cards[i:i+1]]
+		if aValue != bValue {
+			diff = aValue - bValue
+			break
+		}
+	}
+	return diff
+}
+
+func SortHandsJoker(a, b Hand) int {
+	if a.Type != b.Type {
+		return int(a.Type - b.Type)
+	}
+
+	diff := 0
+	for i := 0; i < len(a.Cards); i++ {
+		aValue := JokerCardLookup[a.Cards[i:i+1]]
+		bValue := JokerCardLookup[b.Cards[i:i+1]]
+		if aValue != bValue {
+			diff = aValue - bValue
+			break
+		}
+	}
+	return diff
 }
--- day07/day07_test.go
+++ day07/day07_test.go
@@ -23,8 +23,36 @@ func TestPart01(t *testing.T) {
 }
 
 func TestPart02(t *testing.T) {
-	expected := ""
+	expected := "5905"
 	actual, _ := day07.Part02(testInput)
+
+	if expected != actual {
+		t.Errorf("Expected %s got %s", expected, actual)
+	}
+
+	testInput := `2345A 1
+Q2KJJ 13
+Q2Q2Q 19
+T3T3J 17
+T3Q33 11
+2345J 3
+J345A 2
+32T3K 5
+T55J5 29
+KK677 7
+KTJJT 34
+QQQJA 31
+JJJJJ 37
+JAAAA 43
+AAAAJ 59
+AAAAA 61
+2AAAA 23
+2JJJJ 53
+JJJJ2 41
+`
+
+	expected = "6839"
+	actual, _ = day07.Part02(testInput)
 
 	if expected != actual {
 		t.Errorf("Expected %s got %s", expected, actual)
@@ -34,6 +62,7 @@ func TestPart02(t *testing.T) {
 func TestParseHand(t *testing.T) {
 	testCases := []struct {
 		input    string
+		useJoker bool
 		expected day07.Hand
 	}{
 		{
@@ -102,11 +131,195 @@ func TestParseHand(t *testing.T) {
 				Type: day07.Threes,
 			},
 		},
+
+		// J as Jokers
+		{
+			input:    "32T3K 765",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "32T3K",
+				Bid:   765,
+				Counts: map[string]int{
+					"2": 1,
+					"3": 2,
+					"T": 1,
+					"K": 1,
+				},
+				Type: day07.OnePair,
+			},
+		},
+		{
+			input:    "T55J5 684",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "T55J5",
+				Bid:   684,
+				Counts: map[string]int{
+					"5": 3,
+					"T": 1,
+					"J": 1,
+				},
+				Type: day07.Fours,
+			},
+		},
+		{
+			input:    "KK677 28",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "KK677",
+				Bid:   28,
+				Counts: map[string]int{
+					"6": 1,
+					"7": 2,
+					"K": 2,
+				},
+				Type: day07.TwoPair,
+			},
+		},
+		{
+			input:    "KTJJT 220",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "KTJJT",
+				Bid:   220,
+				Counts: map[string]int{
+					"T": 2,
+					"J": 2,
+					"K": 1,
+				},
+				Type: day07.Fours,
+			},
+		},
+		{
+			input:    "QQQJA 483",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "QQQJA",
+				Bid:   483,
+				Counts: map[string]int{
+					"J": 1,
+					"Q": 3,
+					"A": 1,
+				},
+				Type: day07.Fours,
+			},
+		},
+		{
+			input: "8J29Q 523",
+			expected: day07.Hand{
+				Cards: "8J29Q",
+				Bid:   523,
+				Counts: map[string]int{
+					"2": 1,
+					"8": 1,
+					"9": 1,
+					"J": 1,
+					"Q": 1,
+				},
+				Type: day07.HighCard,
+			},
+		},
+		{
+			input:    "8J29Q 523",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "8J29Q",
+				Bid:   523,
+				Counts: map[string]int{
+					"2": 1,
+					"8": 1,
+					"9": 1,
+					"J": 1,
+					"Q": 1,
+				},
+				Type: day07.OnePair,
+			},
+		},
+		{
+			input:    "TAJA2 998",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "TAJA2",
+				Bid:   998,
+				Counts: map[string]int{
+					"2": 1,
+					"T": 1,
+					"J": 1,
+					"A": 2,
+				},
+				Type: day07.Threes,
+			},
+		},
+		{
+			input: "JJJJJ 145",
+			expected: day07.Hand{
+				Cards: "JJJJJ",
+				Bid:   145,
+				Counts: map[string]int{
+					"J": 5,
+				},
+				Type: day07.Fives,
+			},
+		},
+		{
+			input:    "JJJJJ 145",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "JJJJJ",
+				Bid:   145,
+				Counts: map[string]int{
+					"J": 5,
+				},
+				Type: day07.Fives,
+			},
+		},
+		{
+			input:    "JQJ55 793",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "JQJ55",
+				Bid:   793,
+				Counts: map[string]int{
+					"J": 2,
+					"Q": 1,
+					"5": 2,
+				},
+				Type: day07.Fours,
+			},
+		},
+		{
+			input:    "9JJJ9 500",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "9JJJ9",
+				Bid:   500,
+				Counts: map[string]int{
+					"J": 3,
+					"9": 2,
+				},
+				Type: day07.Fives,
+			},
+		},
+		{
+			input:    "AA55J 500",
+			useJoker: true,
+			expected: day07.Hand{
+				Cards: "AA55J",
+				Bid:   500,
+				Counts: map[string]int{
+					"J": 1,
+					"A": 2,
+					"5": 2,
+				},
+				Type: day07.FullHouse,
+			},
+		},
 	}
+
 	for _, tC := range testCases {
 		t.Run(tC.input, func(t *testing.T) {
 
-			actual, err := day07.ParseHand(&tC.input)
+			actual, err := day07.ParseHand(&tC.input, tC.useJoker)
 
 			if err != nil {
 				t.Error(err)
